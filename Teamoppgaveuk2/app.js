9983761974b9e11daa02942161d573c34c0fbbbd
griminir <viktor.elliot.king@gmail.com>
2024-01-31 14:00:41 +0100

    finished
    uke 2 team oppgave

--- Teamoppgaveuk2/app.js
+++ Teamoppgaveuk2/app.js
@@ -2,26 +2,61 @@ let inventory = false;
 let item = '0';
 let soldItemId = '';
 
-function openInv() {
+function toggleInv() {
   if (inventory === false) {
-    document.getElementById('inventory').innerHTML = /*html*/ `
-  <div class='inventoryOpen'><img class="item" src="img/item${item}.gif"/></div>`;
-    inventory = !inventory;
+    openInv();
   } else {
-    document.getElementById('inventory').innerHTML = /*html0*/ `
-    <div class="inventoryClosed"></div>`;
+    closeInv();
+  }
+}
+
+function openInv() {
+  for (let i = 1; i < 2; i++) {
+    document.getElementById('inventory').innerHTML += /*html*/ `
+  <div class='inventoryOpen'><img class="item" src="img/item${item}.gif"/></div>`;
     inventory = !inventory;
   }
 }
 
+function closeInv() {
+  document.getElementById('inventory').innerHTML = /*html*/ `
+    <div class="inventoryClosed"></div>`;
+  inventory = !inventory;
+}
+
 function getItem(number) {
-  item = number;
-  soldItemId = number;
-  soldItem();
+  if (number === 4) {
+    document.querySelector('.hp').innerHTML = /*html*/ `
+      <img src="img/damageHpBar.jpg" alt="damage Hp">
+    `;
+    item = number;
+    soldItemId = number;
+    soldItem();
+    openInv();
+  } else if (number === 3) {
+    document.querySelector('.hp').innerHTML = /*html*/ `
+      <img src="img/hpbarfull.jpg" alt="damage Hp">
+    `;
+    item = number;
+    soldItemId = number;
+    soldItem();
+    openInv();
+  } else {
+    item = number;
+    soldItemId = number;
+    soldItem();
+    openInv();
+  }
 }
 
 function soldItem() {
   document.getElementById(soldItemId).innerHTML = /*html*/ `
   <img class="item" src="img/sold.png" alt="sold" />
+  `;
+}
+
+function speak() {
+  document.querySelector('#pitch').innerHTML = /*html*/ `
+    <span>"DO NOT DUPLICATE MY ITEMS!!"<span>
   `;
 }
